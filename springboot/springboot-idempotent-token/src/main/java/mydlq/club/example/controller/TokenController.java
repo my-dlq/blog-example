b17552b8691e877000fe2dcaea54e35ad9bcead2
my-dlq <mynamedlq@163.com>
2020-11-12 18:32:50 +0800

    修改注解描述错误

--- springboot/springboot-idempotent-token/src/main/java/mydlq/club/example/controller/TokenController.java
+++ springboot/springboot-idempotent-token/src/main/java/mydlq/club/example/controller/TokenController.java
@@ -27,7 +27,7 @@ public class TokenController {
         // 获取用户信息（这里使用模拟数据）
         // 注：这里存储该内容只是举例，其作用为辅助验证，使其验证逻辑更安全，如这里存储用户信息，其目的为:
         // 1)、使用"token"验证 Redis 中是否存在对应的 Key
-        // 2)、使用"用户信息"验证验证 Redis 的 Value 是否匹配。
+        // 2)、使用"用户信息"验证 Redis 的 Value 是否匹配。
         String userInfo = "mydlq";
         // 获取 Token 字符串，并返回
         return tokenService.generateToken(userInfo);
