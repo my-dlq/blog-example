24ad67d114952e983d6fd3891a2eedcec094b78d
my-dlq <mynamedlq@163.com>
2022-01-04 17:20:03 +0800

    修改加锁实现逻辑,修正错误部分

--- springboot/springboot-data-redis-lock-example/src/main/java/mydlq/club/lock/controller/LockController.java
+++ springboot/springboot-data-redis-lock-example/src/main/java/mydlq/club/lock/controller/LockController.java
@@ -4,6 +4,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import java.util.UUID;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
@@ -30,17 +31,39 @@ public class LockController {
 
     @GetMapping("/lock")
     public void lockTest() {
-        for (int i = 0; i < 1000; i++) {
+        for (int i = 0; i < 100; i++) {
             executor.submit(() -> {
-                // 获取分布式锁，设置超时时间为 10 秒
-                boolean execute = redisLock.tryLock("test", "", 10, TimeUnit.SECONDS);
+                // 生成 6 位的 UUID，将该 UUID 作为 Redis 的值。
+                // 注：设置个 UUID 随机值充当 Redis 存入的 Value 是为了保证，在分布式环境且存在多实例情况下，
+                // 进行加锁和解锁操作的都是相同的进程（同一个实例），这样能够避免该锁被别的进程（实例）执行解锁操作。
+                String value = UUID.randomUUID().toString().substring(0, 6);
+                // 获取分布式锁，设置锁超时时间为 10 秒
+                boolean execute = redisLock.lock("test", value, 10, TimeUnit.SECONDS);
                 // 如果获取锁成功，则执行对应逻辑
                 if (execute) {
-                    log.info("获取分布式锁，执行对应逻辑1");
-                    log.info("获取分布式锁，执行对应逻辑2");
-                    log.info("获取分布式锁，执行对应逻辑3");
+                    log.info("线程 {} - 获取分布式锁，执行对应逻辑", Thread.currentThread().getName());
                     // 执行完成，释放分布式锁
-                    redisLock.unLock("test","");
+                    redisLock.unLock("test", value);
+                }
+            });
+        }
+    }
+
+    @GetMapping("/tryLock")
+    public void tryLockTest() {
+        for (int i = 0; i < 10; i++) {
+            executor.submit(() -> {
+                // 生成 6 位的 UUID，将该 UUID 作为 Redis 的值。
+                // 注：设置个 UUID 随机值充当 Redis 存入的 Value 是为了保证，在分布式环境且存在多实例情况下，
+                // 进行加锁和解锁操作的都是相同的进程（同一个实例），这样能够避免该锁被别的进程（实例）执行解锁操作。
+                String value = UUID.randomUUID().toString().substring(0, 6);
+                // 获取分布式锁，设置锁超时时间为 10 秒,尝试获取锁的时间为 2 秒(2秒内没有获取锁就会获取锁失败)
+                boolean execute = redisLock.tryLock("test", value, 10, TimeUnit.SECONDS, 2, TimeUnit.SECONDS);
+                // 如果获取锁成功，则执行对应逻辑
+                if (execute) {
+                    log.info("线程 {} - 获取分布式锁，执行对应逻辑", Thread.currentThread().getName());
+                    // 执行完成，释放分布式锁
+                    redisLock.unLock("test", value);
                 }
             });
         }
--- springboot/springboot-data-redis-lock-example/src/main/java/mydlq/club/lock/controller/RedisLock.java
+++ springboot/springboot-data-redis-lock-example/src/main/java/mydlq/club/lock/controller/RedisLock.java
@@ -10,6 +10,8 @@ import java.util.Arrays;
 import java.util.concurrent.TimeUnit;
 
 /**
+ * Redis 锁实现
+ *
  * @author mydlq
  */
 @Slf4j
@@ -23,21 +25,56 @@ public class RedisLock {
     private StringRedisTemplate redisTemplate;
 
     /**
-     * 尝试获取分布式锁
+     * 获取分布式锁
      *
-     * @param key        分布式锁 key
-     * @param value      分布式锁 value
-     * @param expireTime 超时时间
-     * @param timeUnit   超时时间单位
+     * @param key            分布式锁 key
+     * @param value          分布式锁 value
+     * @param expireTime     锁的超时时间,防止死锁
+     * @param expireTimeUnit 锁的超时时间单位
      * @return 是否成功获取分布式锁
      */
-    public boolean tryLock(String key, String value, int expireTime, TimeUnit timeUnit) {
-        Boolean result = redisTemplate.opsForValue().setIfAbsent(key, value, expireTime, timeUnit);
+    public boolean lock(String key, String value, int expireTime, TimeUnit expireTimeUnit) {
+        Boolean result = redisTemplate.opsForValue().setIfAbsent(key, value, expireTime, expireTimeUnit);
         if (Boolean.TRUE.equals(result)) {
-            log.error("申请锁(" + key + "|" + value + ")成功");
+            log.info("线程 {} - 申请锁( {} | {} )成功", Thread.currentThread().getName(), key, value);
             return true;
         }
-        log.error("申请锁(" + key + "|" + value + ")失败");
+        log.info("线程 {} - 申请锁( {} | {} )失败", Thread.currentThread().getName(), key, value);
+        return false;
+    }
+
+    /**
+     * 尝试获取分布式锁,并设置获取锁的超时时间
+     *
+     * @param key                分布式锁 key
+     * @param value              分布式锁 value
+     * @param expireTime         锁的超时时间,防止死锁
+     * @param expireTimeUnit     锁的超时时间单位
+     * @param acquireTimeout     尝试获取锁的等待时间,如果在时间范围内获取锁失败,就结束获取锁
+     * @param acquireTimeoutUnit 尝试获取锁的等待时间单位
+     * @return 是否成功获取分布式锁
+     */
+    public boolean tryLock(String key, String value, int expireTime, TimeUnit expireTimeUnit, int acquireTimeout, TimeUnit acquireTimeoutUnit) {
+        try {
+            // 尝试自旋获取锁,等待配置的一段时间,如果在时间范围内获取锁失败,就结束获取锁
+            long end = System.currentTimeMillis() + acquireTimeoutUnit.toMillis(acquireTimeout);
+            while (System.currentTimeMillis() < end) {
+                // 尝试获取锁
+                Boolean result = redisTemplate.opsForValue().setIfAbsent(key, value, expireTime, expireTimeUnit);
+                // 验证是否成功获取锁
+                if (Boolean.TRUE.equals(result)) {
+                    log.info("线程 {} - 申请锁( {} | {} )成功", Thread.currentThread().getName(), key, value);
+                    return true;
+                }
+                // 睡眠 50 毫秒
+                Thread.sleep(50);
+            }
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        } catch (Exception e) {
+            log.error("申请锁(" + key + "|" + value + ")错误:", e);
+        }
+        log.info("线程 {} - 申请锁( {} | {} )失败", Thread.currentThread().getName(), key, value);
         return false;
     }
 
@@ -52,9 +89,9 @@ public class RedisLock {
         RedisScript<Long> redisScript = new DefaultRedisScript<>(script, Long.class);
         Long result = redisTemplate.execute(redisScript, Arrays.asList(key, value));
         if (result != null && result != 0L) {
-            log.info("解锁(" + key + "|" + value + ")成功");
+            log.info("线程 {} - 解锁({} | {}})成功", Thread.currentThread().getName(), key, value);
         } else {
-            log.info("解锁(" + key + "|" + value + ")失败！");
+            log.info("线程 {} - 解锁({} | {})失败！", Thread.currentThread().getName(), key, value);
         }
     }
 
